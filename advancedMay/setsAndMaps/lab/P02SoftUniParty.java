b8e1bbf68be23f07f134bf1383a75412c8a4c5b7
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-05-09 11:07:41 +0300

    Add lab exercises from Maps and Sets lectures

---
+++ advancedMay/setsAndMaps/lab/P02SoftUniParty.java
@@ -0,0 +1,34 @@
+package SoftUni.advancedMay.setsAndMaps.lab;
+
+import java.nio.file.LinkPermission;
+import java.util.*;
+
+public class P02SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> vip = new TreeSet<>();
+        Set<String> regular = new TreeSet<>();
+        String guestId = scanner.nextLine();
+
+        while (!guestId.equals("PARTY")) {
+            if (Character.isDigit(guestId.charAt(0)))
+                vip.add(guestId);
+            else
+                regular.add(guestId);
+            guestId = scanner.nextLine();
+        }
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("END")) {
+            vip.remove(input);
+            regular.remove(input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(vip.size() + regular.size());
+        vip.forEach(System.out::println);
+        regular.forEach(System.out::println);
+    }
+}
