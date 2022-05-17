2d7a1f74d349a0b92671900db6d826d2b8fb4cd0
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-05-17 15:33:27 +0300

    Add lab exercises from Maps and Sets lectures

---
+++ advancedMay/setsAndMaps/exercises/P05Phonebook.java
@@ -0,0 +1,11 @@
+package SoftUni.advancedMay.setsAndMaps.exercises;
+
+import java.util.Scanner;
+
+public class P05Phonebook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
