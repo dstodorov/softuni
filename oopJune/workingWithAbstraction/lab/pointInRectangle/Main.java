248ccbf0fdac6f9db3e9a053270c2a7793b5f49e
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-06-27 18:08:43 +0300

    Add Student Repository and Point in Rectangle from OPP lab Working With Abstraction

---
+++ oopJune/workingWithAbstraction/lab/pointInRectangle/Main.java
@@ -0,0 +1,22 @@
+package SoftUni.oopJune.workingWithAbstraction.lab.pointInRectangle;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] rectangleCoordinates = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        Rectangle rectangle = new Rectangle(new Point(rectangleCoordinates[0], rectangleCoordinates[1]), new Point(rectangleCoordinates[2], rectangleCoordinates[3]));
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            int[] pointCoordinates = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            Point point = new Point(pointCoordinates[0], pointCoordinates[1]);
+            System.out.println(rectangle.contains(point));
+        }
+
+    }
+}
---
+++ oopJune/workingWithAbstraction/lab/studentRepository/Student.java
@@ -0,0 +1,50 @@
+package SoftUni.oopJune.workingWithAbstraction.lab.studentRepository;
+
+public class Student {
+    private String name;
+    private int age;
+    private double grade;
+    private String commentary;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.commentary = getCommentary(grade);
+    }
+
+    private String getCommentary(double grade) {
+        if (grade > 5) return "Excellent student";
+        else if (grade < 5.00 && grade >= 3.50) return "Average student";
+        else return "Very nice person";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is %d years old. %s.", this.name, this.age, this.commentary);
+    }
+}
