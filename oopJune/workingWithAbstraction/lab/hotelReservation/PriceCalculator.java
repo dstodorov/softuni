7aca62f9c05bae2544a2b11b12e9c3cd98370da8
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-06-27 23:00:44 +0300

    Refactor calculation class

--- oopJune/workingWithAbstraction/lab/hotelReservation/PriceCalculator.java
+++ oopJune/workingWithAbstraction/lab/hotelReservation/PriceCalculator.java
@@ -2,41 +2,8 @@ package SoftUni.oopJune.workingWithAbstraction.lab.hotelReservation;
 
 public class PriceCalculator {
     static double calculate(double pricePerDay, int numberOfDays, String vacationSeason, String discountType) {
-        Season season;
-        Discount discount;
-
-        switch (vacationSeason) {
-            case "Summer":
-                season = Season.SUMMER;
-                break;
-            case "Autumn":
-                season = Season.AUTUMN;
-                break;
-            case "Spring":
-                season = Season.SPRING;
-                break;
-            case "Winter":
-                season = Season.WINTER;
-                break;
-            default:
-                season = Season.SUMMER;
-                break;
-        }
-
-        switch (discountType) {
-            case "VIP":
-                discount = Discount.VIP;
-                break;
-            case "SecondVisit":
-                discount = Discount.SECOND_VISIT;
-                break;
-            case "None":
-                discount = Discount.NONE;
-                break;
-            default:
-                discount = Discount.NONE;
-                break;
-        }
+        Season season = Season.valueOf(vacationSeason);
+        Discount discount = Discount.valueOf(discountType);
 
         double priceWithoutDiscount = (pricePerDay * season.getMultiplier()) * numberOfDays;
 
