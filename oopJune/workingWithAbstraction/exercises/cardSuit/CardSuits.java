454cb8a9ceee9a0ba8604e963d47a1ea8aae1aad
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-06-28 10:05:12 +0300

    add card suit exercise

---
+++ oopJune/workingWithAbstraction/exercises/cardSuit/CardSuits.java
@@ -0,0 +1,24 @@
+package SoftUni.oopJune.workingWithAbstraction.exercises.cardSuit;
+
+public enum CardSuits {
+
+    CLUBS(0),
+    DIAMONDS(1),
+    HEARTS(2),
+    SPADES(3);
+
+    private int intValue;
+    private String nameValue;
+
+    CardSuits(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public String getNameValue() {
+        return nameValue;
+    }
+}
---
+++ oopJune/workingWithAbstraction/exercises/cardSuit/Main.java
@@ -0,0 +1,16 @@
+package SoftUni.oopJune.workingWithAbstraction.exercises.cardSuit;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        System.out.println(input + ":");
+        for (CardSuits cardSuit : CardSuits.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardSuit.getIntValue(), cardSuit.name());
+        }
+    }
+}
