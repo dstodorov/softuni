54978be4a3af6aa27aa4a8435c09bb9941766728
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-03 18:54:40 +0300

    Add Hierarchical Inheritance exercise

---
+++ advancedMay/oop/inheritance/lab/hierarchicalInheritance/Animal.java
@@ -0,0 +1,7 @@
+package SoftUni.advancedMay.oop.inheritance.lab.hierarchicalInheritance;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ advancedMay/oop/inheritance/lab/hierarchicalInheritance/Dog.java
@@ -0,0 +1,7 @@
+package SoftUni.advancedMay.oop.inheritance.lab.hierarchicalInheritance;
+
+public class Dog extends Animal {
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
