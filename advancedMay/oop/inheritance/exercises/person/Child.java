1ae4aac9b05ca4cad6bf13450f7ae9b159117593
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-04 15:19:12 +0300

    Add Person exercise

---
+++ advancedMay/oop/inheritance/exercises/person/Child.java
@@ -0,0 +1,9 @@
+package SoftUni.advancedMay.oop.inheritance.exercises.person;
+
+import person.Person;
+
+public class Child extends Person {
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
