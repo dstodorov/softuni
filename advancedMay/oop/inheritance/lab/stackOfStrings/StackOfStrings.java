9968496c2796ae08d3e44392a8e94a79a80978ad
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-04 14:56:24 +0300

    Add Stack of Strings exercise

---
+++ advancedMay/oop/inheritance/lab/stackOfStrings/StackOfStrings.java
@@ -0,0 +1,31 @@
+package SoftUni.advancedMay.oop.inheritance.lab.stackOfStrings;
+
+import java.util.ArrayList;
+
+public class StackOfStrings {
+    private ArrayList<String> data;
+
+    public StackOfStrings() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(String item) {
+        data.add(item);
+    }
+
+    public String pop() {
+        String item = this.data.get(this.data.size() - 1);
+        this.data.remove(item);
+        return item;
+    }
+
+    public String peek() {
+        return this.data.get(this.data.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return this.data.isEmpty();
+    }
+
+
+}
