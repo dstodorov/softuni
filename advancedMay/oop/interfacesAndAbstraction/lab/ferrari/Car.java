40c6781c7235b1ceb1f6b2790c8bf9a79c67afd0
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-06 10:26:24 +0300

    Add Ferrari exercise

---
+++ advancedMay/oop/interfacesAndAbstraction/lab/ferrari/Car.java
@@ -0,0 +1,9 @@
+package SoftUni.advancedMay.oop.interfacesAndAbstraction.lab.ferrari;
+
+public interface Car {
+    String brakes();
+    String gas();
+
+    String MODEL = "488-Spider";
+
+}
---
+++ advancedMay/oop/interfacesAndAbstraction/lab/ferrari/Ferrari.java
@@ -0,0 +1,26 @@
+package SoftUni.advancedMay.oop.interfacesAndAbstraction.lab.ferrari;
+
+public class Ferrari implements Car {
+    private String driverName;
+    private String model;
+
+    public Ferrari(String driverName) {
+        this.driverName = driverName;
+        this.model = Car.MODEL;
+    }
+
+    @Override
+    public String brakes() {
+        return "Brakes!";
+    }
+
+    @Override
+    public String gas() {
+        return "brum-brum-brum-brrrrr";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s/%s/%s", MODEL, brakes(), gas(), this.driverName);
+    }
+}
