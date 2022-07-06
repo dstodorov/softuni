d99c4399c467536efea6eacdc522607606cf8453
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-06 10:34:41 +0300

    Add Interface Person exercise

---
+++ advancedMay/oop/interfacesAndAbstraction/exercises/interfacePerson/Main.java
@@ -0,0 +1,7 @@
+package SoftUni.advancedMay.oop.interfacesAndAbstraction.exercises.interfacePerson;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ advancedMay/oop/interfacesAndAbstraction/exercises/interfacePerson/Person.java
@@ -0,0 +1,6 @@
+package SoftUni.advancedMay.oop.interfacesAndAbstraction.exercises.interfacePerson;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
