f9e69a965c408f3d09c83b1ff259fcc085cd0127
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-08 16:16:43 +0300

    Add math operation exercise

---
+++ advancedMay/oop/polymorphism/lab/mathOperation/Main.java
@@ -0,0 +1,9 @@
+package SoftUni.advancedMay.oop.polymorphism.lab.mathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MathOperation.add(2,2));
+        System.out.println(MathOperation.add(3,3,3));
+        System.out.println(MathOperation.add(4,4,4,4));
+    }
+}
---
+++ advancedMay/oop/polymorphism/lab/mathOperation/MathOperation.java
@@ -0,0 +1,15 @@
+package SoftUni.advancedMay.oop.polymorphism.lab.mathOperation;
+
+public class MathOperation {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int add(int a, int b, int c, int d) {
+        return a + b + c +d;
+    }
+}
