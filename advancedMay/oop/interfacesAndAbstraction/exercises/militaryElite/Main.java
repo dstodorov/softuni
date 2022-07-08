733c41b83782e6ed36e078fab424a9cc30df2528
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-08 10:50:45 +0300

    Refactor main method

--- advancedMay/oop/interfacesAndAbstraction/exercises/militaryElite/Main.java
+++ advancedMay/oop/interfacesAndAbstraction/exercises/militaryElite/Main.java
@@ -15,67 +15,100 @@ public class Main {
 
         while (!input.equals("End")) {
             String[] inputParams = input.split("\\s+");
+
             String soldierType = inputParams[0];
             int id = Integer.parseInt(inputParams[1]);
             String soldierFirstName = inputParams[2];
             String soldierLastName = inputParams[3];
 
-
             switch (soldierType) {
-                case "Private": {
+                case "Private":
                     double soldierSalary = Double.parseDouble(inputParams[4]);
-                    PrivateImpl priv = new PrivateImpl(id, soldierFirstName, soldierLastName, soldierSalary);
-                    privates.add(priv);
-                    System.out.println(priv);
-                    break;
-                }
-                case "LieutenantGeneral": {
-                    double soldierSalary = Double.parseDouble(inputParams[4]);
-                    List<PrivateImpl> team = new ArrayList<>();
-                    for (int i = 5; i < inputParams.length; i++) {
-                        int privateId = Integer.parseInt(inputParams[i]);
-                        PrivateImpl priv = privates.stream().filter(p -> p.getId() == privateId).findFirst().orElse(null);
-                        team.add(priv);
-                    }
-                    LieutenantGeneralImpl lieutenantGeneral = new LieutenantGeneralImpl(id, soldierFirstName, soldierLastName, soldierSalary, team);
-                    System.out.println(lieutenantGeneral);
-                    break;
-                }
-                case "Engineer": {
-                    double soldierSalary = Double.parseDouble(inputParams[4]);
-                    String soldierCorps = inputParams[5];
-                    List<Repair> repairs = new ArrayList<>();
-                    for (int i = 6; i < inputParams.length; i += 2) {
-                        Repair repair = new Repair(inputParams[i], Integer.parseInt(inputParams[i + 1]));
-                        repairs.add(repair);
-                    }
-                    EngineerImpl engineer = new EngineerImpl(id, soldierFirstName, soldierLastName, soldierSalary, soldierCorps, repairs);
-                    if (!engineer.getCorps().equals("invalid")) System.out.println(engineer);
-                    break;
-                }
-                case "Commando": {
-                    double soldierSalary = Double.parseDouble(inputParams[4]);
-                    String soldierCorps = inputParams[5];
-                    List<Mission> missions = new ArrayList<>();
-                    for (int i = 6; i < inputParams.length; i += 2) {
-                        Mission mission = new Mission(inputParams[i], inputParams[i + 1]);
-                        if (!mission.getState().equals("invalid")) missions.add(mission);
-                    }
 
-                    CommandoImpl commando = new CommandoImpl(id, soldierFirstName, soldierLastName, soldierSalary, soldierCorps, missions);
-                    if (!commando.getCorps().equals("invalid")) System.out.println(commando);
+                    addPrivate(id, soldierFirstName, soldierLastName, soldierSalary, privates);
                     break;
+
+                case "LieutenantGeneral":
+                    double LieutenantGeneralSalary = Double.parseDouble(inputParams[4]);
+
+                    List<PrivateImpl> team = addLieutenantTeam(privates, inputParams);
+                    LieutenantGeneralImpl lieutenantGeneral = new LieutenantGeneralImpl(id, soldierFirstName, soldierLastName, LieutenantGeneralSalary, team);
+                    printSoldier(lieutenantGeneral);
+                    break;
+
+                case "Engineer":
+                    double engineerSalary = Double.parseDouble(inputParams[4]);
+
+                    String engineerCorps = inputParams[5];
+                    List<Repair> repairs = getRepairs(inputParams);
+                    EngineerImpl engineer = new EngineerImpl(id, soldierFirstName, soldierLastName, engineerSalary, engineerCorps, repairs);
+
+                    if (validCorps(engineer.getCorps())) printSoldier(engineer);
+                    break;
+
+                case "Commando": {
+                    double commandoSalary = Double.parseDouble(inputParams[4]);
+
+                    String soldierCorps = inputParams[5];
+                    List<Mission> missions = getMissions(inputParams);
+                    CommandoImpl commando = new CommandoImpl(id, soldierFirstName, soldierLastName, commandoSalary, soldierCorps, missions);
+
+                    if (validCorps(commando.getCorps())) printSoldier(commando);
+                    break;
+
                 }
-                case "Spy": {
+                case "Spy":
                     String soldierCodeNumber = inputParams[4];
+
                     Spy spy = new SpyImpl(id, soldierFirstName, soldierLastName, soldierCodeNumber);
-                    System.out.println(spy);
+                    printSoldier(spy);
                     break;
-                }
 
             }
 
             input = scanner.nextLine();
         }
+    }
+
+    private static boolean validCorps(String corps) {
+        return !corps.equals("invalid");
+    }
+
+    private static <E> void printSoldier(E soldierObject) {
+        System.out.println(soldierObject);
+    }
+
+    private static List<Mission> getMissions(String[] inputParams) {
+        List<Mission> missions = new ArrayList<>();
+        for (int i = 6; i < inputParams.length; i += 2) {
+            Mission mission = new Mission(inputParams[i], inputParams[i + 1]);
+            if (!mission.getState().equals("invalid")) missions.add(mission);
+        }
+        return missions;
+    }
+
+    private static List<Repair> getRepairs(String[] inputParams) {
+        List<Repair> repairs = new ArrayList<>();
+        for (int i = 6; i < inputParams.length; i += 2) {
+            Repair repair = new Repair(inputParams[i], Integer.parseInt(inputParams[i + 1]));
+            repairs.add(repair);
+        }
+        return repairs;
+    }
+
+    private static List<PrivateImpl> addLieutenantTeam(List<PrivateImpl> privates, String[] soldiersInfo) {
+        List<PrivateImpl> team = new ArrayList<>();
+        for (int i = 5; i < soldiersInfo.length; i++) {
+            int privateId = Integer.parseInt(soldiersInfo[i]);
+            PrivateImpl priv = privates.stream().filter(p -> p.getId() == privateId).findFirst().orElse(null);
+            team.add(priv);
+        }
+        return team;
+    }
+
+    private static void addPrivate(int id, String soldierFirstName, String soldierLastName, double soldierSalary, List<PrivateImpl> privates) {
+        PrivateImpl priv = new PrivateImpl(id, soldierFirstName, soldierLastName, soldierSalary);
+        privates.add(priv);
+        System.out.println(priv);
     }
 }
