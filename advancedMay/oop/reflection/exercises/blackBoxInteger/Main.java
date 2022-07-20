b86a2e4fbc805f65038a8f80d3216c8a38816211
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-07-20 14:37:31 +0300

    Refactor Black Box Integer exercise

--- advancedMay/oop/reflection/exercises/blackBoxInteger/Main.java
+++ advancedMay/oop/reflection/exercises/blackBoxInteger/Main.java
@@ -15,57 +15,33 @@ public class Main {
         ctor.setAccessible(true);
         BlackBoxInt myObject = (BlackBoxInt) ctor.newInstance(0);
 
-        Method add = myObject.getClass().getDeclaredMethod("add", int.class);
-        Method subtract = myObject.getClass().getDeclaredMethod("subtract", int.class);
-        Method multiply = myObject.getClass().getDeclaredMethod("multiply", int.class);
-        Method divide = myObject.getClass().getDeclaredMethod("divide", int.class);
-        Method leftShift = myObject.getClass().getDeclaredMethod("leftShift", int.class);
-        Method rightShift = myObject.getClass().getDeclaredMethod("rightShift", int.class);
-        add.setAccessible(true);
-        subtract.setAccessible(true);
-        multiply.setAccessible(true);
-        divide.setAccessible(true);
-        leftShift.setAccessible(true);
-        rightShift.setAccessible(true);
-
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
 
         while (!input.equals("END")) {
             String tokens[] = input.split("_");
-            String command = tokens[0];
-            int value = Integer.parseInt(tokens[1]);
 
-            switch (command) {
-                case "add":
-                    add.invoke(myObject, value);
-                    break;
-                case "subtract":
-                    subtract.invoke(myObject, value);
-                    break;
-                case "multiply":
-                    multiply.invoke(myObject, value);
-                    break;
-                case "divide":
-                    divide.invoke(myObject, value);
-                    break;
-                case "leftShift":
-                    leftShift.invoke(myObject, value);
-                    break;
-                case "rightShift":
-                    rightShift.invoke(myObject, value);
-                    break;
-            }
+            int result = getCommandResult(myObject, tokens);
 
-            Field innerValue = myObject.getClass().getDeclaredField("innerValue");
-            innerValue.setAccessible(true);
-            Object val = innerValue.get(myObject);
-            System.out.println(val);
+            System.out.println(result);
 
             input = scanner.nextLine();
         }
+    }
 
+    private static int getCommandResult(BlackBoxInt myObject, String[] tokens) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        String command = tokens[0];
+        int value = Integer.parseInt(tokens[1]);
 
+        Method operation = myObject.getClass().getDeclaredMethod(command, int.class);
+        operation.setAccessible(true);
+        operation.invoke(myObject, value);
+
+        Field innerValue = myObject.getClass().getDeclaredField("innerValue");
+        innerValue.setAccessible(true);
+        Object val = innerValue.get(myObject);
+
+        return (int) val;
     }
 }
